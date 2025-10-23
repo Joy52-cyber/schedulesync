1b32e15e328f4c789ad22187d56abc526e01d892
Joyla <joylacaba52@gmail.com>
2025-10-23 15:56:02 +0800

    Add debug endpoint

--- server.js
+++ server.js
@@ -685,6 +685,7 @@ app.get('/api/config/status', (_req, res) => {
 });
 
 /* ------------------------------- Static Pages ----------------------------- */
+app.get('/', (_req, res) => res.sendFile(path.join(__dirname, 'public', 'index.html')));
 app.get('/login', (_req, res) => res.sendFile(path.join(__dirname, 'public', 'login.html')));
 app.get('/dashboard', (_req, res) => res.sendFile(path.join(__dirname, 'public', 'dashboard.html')));
 app.get('/booking', (_req, res) => res.sendFile(path.join(__dirname, 'public', 'booking.html')));
